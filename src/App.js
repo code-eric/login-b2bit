56a2fdbaf3360971ea6cc840c677a61bbb9cc8e1
Eric Andrade <e.andrade5f@gmail.com>
2022-03-11 20:45:11 -0300

    adaptando para b2bit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import LoginForm from './components/LoginForm';
 
 function App() {
   const adminUser = {
@@ -7,28 +8,59 @@ function App() {
   }
 
 
-  const [user, setUser] = useState({name:"",password:""});
-  const [error, setError] = useState("")
+  const [user, setUser] = useState({email:""});
+  const [error, setError] = useState("");
 
   const Login = details => {
-    console.log(details);
+    //console.log(details);
+    
+
+    /*
+    if(details.username === adminUser.username && details.password === adminUser.password){
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    }else{
+      setError("details do not match")
+    }
+    */
+    console.log(details)
+    fetch('https://frontendproject.b2bit.company/account/tokens/',{
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(details)
+    }).then((response) => {
+      if (response.ok) {
+        setUser({
+          name: details.name,
+          email: details.email
+        })
+      }else{
+        setError("details do not match")
+      }
+      
+    })
+
+
   }
 
   const Logout = () => {
-    console.log("logout");
+    setUser({email:""});
+    setError("")
   }
 
   return (
     
     <div className="App">
-      {(user.email != "") ?(
+      {(user.email != "") ? (
         <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button>Logout</button>        
-        </div>):(
-          <LoginForm />
-        )
-      }
+          <h2>Email is: <span>{user.email}</span></h2>
+          <button onClick={Logout}>Logout</button>        
+        </div>
+        ) : (
+          <LoginForm Login={Login} error={error}/>
+        )}
     </div>
   );
 }
