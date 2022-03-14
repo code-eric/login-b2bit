bcb44c1293f571e8a9f07d3288b79d78c1de04e4
Eric Andrade <e.andrade5f@gmail.com>
2022-03-14 09:09:44 -0300

    logged in info

--- src/App.js
+++ src/App.js
@@ -2,59 +2,59 @@ import React, {useState} from 'react'
 import LoginForm from './components/LoginForm';
 
 function App() {
-  const adminUser = {
-    admin: "admin@gmail.com",
-    password: "1234"
-  }
-
-
-  const [user, setUser] = useState({email:""});
+  const [loginEmail, setLoginEmail] = useState("");
   const [error, setError] = useState("");
   const [access, setAccess] = useState("");
   const [refresh, setRefresh] = useState("");
+  const [name, setName] = useState("");
+  const [registeredEmail, setRegisteredEmail] = useState("");
+  const [pictureLink, setPictureLink] = useState("");
+
+
+  let test = 1;
   
-  const Login = details => {
+  const Login = async details => {
     
-  let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  
-  let raw = JSON.stringify({
-    "email": details.email,
-    "password": details.password
-  });
-  
-  let requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-  
-  fetch("https://frontendproject.b2bit.company/account/tokens/", requestOptions)
-    .then(response => response.text())
-    .then(result => JSON.parse(result))
-    .then(result =>{
-      console.log(result)
-      setAccess(result.tokens.access)
-      setRefresh(result.tokens.refresh)
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    let raw = JSON.stringify({
+      "email": details.email,
+      "password": details.password
+    });
+    
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
       
-    })
-    .then(console.log(access))
-    .catch(error => console.log('error', error));
-}
+    const response = await fetch("https://frontendproject.b2bit.company/account/tokens/", requestOptions);
+    const text = await response.text();
+    const json = await JSON.parse(text);
+    setAccess(json.tokens.access);
+    setRefresh(json.tokens.refresh);
+    setPictureLink(json.user.avatar.image_medium_url);
+    setName(`${json.user.name} ${json.user.last_name}`);
+    setLoginEmail(json.user.email);
+
+    console.log(name, pictureLink);
+  }
   
 
   const Logout = () => {
-    setUser({email:""});
+    setLoginEmail("");
     setError("")
   }
 
   return (
     
     <div className="App">
-      {(user.email != "") ? (
+      {(loginEmail != "") ? (
         <div className="welcome">
-          <h2>Email is: <span>{user.email}</span></h2>
+          <h2>Email is: <span>{loginEmail}</span></h2>
           <button onClick={Logout}>Logout</button>        
         </div>
         ) : (
